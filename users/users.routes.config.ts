466cc0e87eef0bbad4a648685f3106ace7be6e7c
Carla Serrano <carlacarly8@gmail.com>
2022-06-22 21:56:09 -0300

    feat: Express validator middleware

--- users/users.routes.config.ts
+++ users/users.routes.config.ts
@@ -3,6 +3,9 @@ import UsersController from "./controllers/users.controller";
 import UsersMiddleware from "./middleware/users.middleware";
 import express from "express";
 
+import BodyValidationMiddleware from "../common/middleware/body.validation.middleware";
+import { body } from "express-validator";
+
 export class UsersRoutes extends CommonRoutesConfig {
   constructor(app: express.Application) {
     super(app, "UsersRoutes");
@@ -14,7 +17,22 @@ export class UsersRoutes extends CommonRoutesConfig {
       .get(UsersController.listUsers)
       .post(
         UsersMiddleware.validateRequiredUserBodyFields,
+        body("email").isEmail(),
+        body("password")
+          .isLength({ min: 5 })
+          .withMessage("Must include password (5+ characters)"),
+        BodyValidationMiddleware.verifyBodyFieldsErrors,
+
         UsersMiddleware.validateSameEmailDoesntExist,
+        body("email").isEmail(),
+        body("password")
+          .isLength({ min: 5 })
+          .withMessage("Must include password (5+ characters)"),
+        body("firstName").isString(),
+        body("lastName").isString(),
+        body("permissionFlags").isInt(),
+        BodyValidationMiddleware.verifyBodyFieldsErrors,
+
         UsersController.createUser
       );
 
@@ -32,8 +50,15 @@ export class UsersRoutes extends CommonRoutesConfig {
     ]);
 
     this.app.patch(`/users/:userId`, [
-      UsersMiddleware.validatePatchEmail,
-      UsersController.patch,
+      body("email").isEmail().optional(),
+      body("password")
+        .isLength({ min: 5 })
+        .withMessage("Password must be 5+ characters")
+        .optional(),
+      body("firstName").isString().optional(),
+      body("lastName").isString().optional(),
+      body("permissionFlags").isInt().optional(),
+      BodyValidationMiddleware.verifyBodyFieldsErrors,
     ]);
 
     return this.app;
