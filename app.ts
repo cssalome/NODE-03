fac0852460105df0002fe96995f0be26940c8380
Carla Serrano <carlacarly8@gmail.com>
2022-06-23 19:11:12 -0300

    feat: ENV added

--- app.ts
+++ app.ts
@@ -1,18 +1,24 @@
-import express from 'express';
-import * as http from 'http';
-import * as winston from 'winston';
-import * as expressWinston from 'express-winston';
-import cors from 'cors';
-import debug from 'debug';
+import express from "express";
+import * as http from "http";
+import * as winston from "winston";
+import * as expressWinston from "express-winston";
+import cors from "cors";
+import debug from "debug";
+import dotenv from "dotenv";
 
-import { CommonRoutesConfig } from './common/common.routes.config';
-import { UsersRoutes } from './users/users.routes.config';
+import { CommonRoutesConfig } from "./common/common.routes.config";
+import { UsersRoutes } from "./users/users.routes.config";
+
+const dotenvResult = dotenv.config();
+if (dotenvResult.error) {
+  throw dotenvResult.error;
+}
 
 const app: express.Application = express();
 const server: http.Server = http.createServer(app);
 const port = 3000;
 const routes: Array<CommonRoutesConfig> = [];
-const debugLog: debug.IDebugger = debug('app');
+const debugLog: debug.IDebugger = debug("app");
 
 // here we are adding middleware to parse all incoming requests as JSON
 app.use(express.json());
@@ -45,7 +51,7 @@ routes.push(new UsersRoutes(app));
 
 // this is a simple route to make sure everything is working properly
 const runningMessage = `Server running at http://localhost:${port}`;
-app.get('/', (req: express.Request, res: express.Response) => {
+app.get("/", (req: express.Request, res: express.Response) => {
   res.status(200).send(runningMessage);
 });
 
